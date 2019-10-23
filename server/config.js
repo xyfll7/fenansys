68f7ee03b0e6e06babc6cc5b3715dba3eec06dc3
xyfll7 <l.7@qq.com>
2019-10-23 20:52:15 +0800

    build

--- server/config.js
+++ server/config.js
@@ -1,10 +1,12 @@
 let mongoHost = '106.13.92.185'
+let mongoURI = `mongodb://${mongoHost}:27018/xinjiangfy`
 if (process.env.NODE_ENV === 'production') {
   mongoHost = 'localHost'
+  mongoURI = `mongodb://${mongoHost}:27018,${mongoHost}:27017,${mongoHost}:27019/xinjiangfy?replicaSet=rep`
 }
 
 module.exports = {
-  mongoURI: `mongodb://${mongoHost}:27017,${mongoHost}:27018,${mongoHost}:27019/xinjiangfy?replicaSet=rep`,
+  mongoURI,
   secretOrKey: 'secret',
   baseApi: '/api/v1'
 }
