bbc1708a2d3f6dcd6a98d540d8e4d998ada9967f
xyfll7 <l.7@qq.com>
2019-10-24 11:11:01 +0800

    build

--- server/login/login.js
+++ server/login/login.js
@@ -1,5 +1,5 @@
 const Koa = require('koa')
-const cors = require('koa2-cors')
+const cors = require('@koa/cors')
 const bodyParser = require('koa-bodyparser')
 const dbConnect = require('./modules/dbconnect')
 const router = require('./router')
