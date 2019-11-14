96086498f888acb6ae8f687fabdb1f53fe4cfa00
xyfll7 <l.7@qq.com>
2019-11-14 14:01:21 +0800

    build

--- server/config.js
+++ server/config.js
@@ -5,7 +5,7 @@ let mongoHost = '148.70.143.157'
 // let mongoURI = `mongodb://${mongoHost}:27028/xinjiangfy`
 // let mongoURI = `mongodb://${mongoUser}:${mongoPass}@${mongoHost}:${port}/xinjiangfy`
 let mongoURI = `mongodb://xyf:yangqi7'@148.70.143.157:27028/xinjiangfy?replicaSet=rep`
-mongoURI = "mongodb://xyf:yangqi7'@localhost:27017,localhost:27018,localhost:27019/xinjiangfy?replicaSet=rep"
+mongoURI = "mongodb://xyf:yangqi7'@localhost:27017,localhost:27018,localhost:27019/xinjiangfy?replicaSet=rep&authSource=admin"
 // let mongoURI = `mongodb://xyf:yangqi7'@${mongoHost}:27029,${mongoHost}:27028,${mongoHost}:27027/xinjiangfy?replicaSet=rep`
 // let mongoURI = `mongodb://${mongoHost}:27027,${mongoHost}:27028,${mongoHost}:27029/xinjiangfy?replicaSet=rep`
 if (process.env.NODE_ENV === 'production') {
