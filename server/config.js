db1ab2a3586311714449582c4761fc8d2bfcf72b
xyfll7 <l.7@qq.com>
2019-11-14 14:20:27 +0800

    build

--- server/config.js
+++ server/config.js
@@ -3,13 +3,12 @@ const mongoPass = "yangqi7'"
 let mongoHost = '148.70.143.157'
 // const port = '27028'
 let mongoURI = `mongodb://xyf:yangqi7'@148.70.143.157:27028/xinjiangfy?&authSource=admin`
-mongoURI = "mongodb://xyf:yangqi7'@localhost:27017,localhost:27018,localhost:27019/xinjiangfy?replicaSet=rep&authSource=admin&ssl=true"
+// mongoURI = "mongodb://xyf:yangqi7'@localhost:27017,localhost:27018,localhost:27019/xinjiangfy?replicaSet=rep&authSource=admin"
 // let mongoURI = `mongodb://xyf:yangqi7'@${mongoHost}:27029,${mongoHost}:27028,${mongoHost}:27027/xinjiangfy?replicaSet=rep`
 // let mongoURI = `mongodb://${mongoHost}:27027,${mongoHost}:27028,${mongoHost}:27029/xinjiangfy?replicaSet=rep`
 if (process.env.NODE_ENV === 'production') {
   mongoHost = 'localHost'
-  mongoURI = `mongodb://${mongoUser}:${mongoPass}@${mongoHost}:27017,${mongoHost}:27018,${mongoHost}:27019/xinjiangfy?replicaSet=rep&authSource=admin&ssl=true`
-  mongoURI = "mongodb://xyf:yangqi7'@localhost:27017,localhost:27018,localhost:27019/xinjiangfy?replicaSet=rep&ssl=true"
+  mongoURI = `mongodb://${mongoUser}:${mongoPass}@${mongoHost}:27017,${mongoHost}:27018,${mongoHost}:27019/xinjiangfy?replicaSet=rep&authSource=admin`
 }
 console.log(mongoURI)
 module.exports = {
